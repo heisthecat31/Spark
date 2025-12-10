534ecc06a8a594a480dc58994c971eb3154b95f7
he_is_the_cat <125207670+heisthecat31@users.noreply.github.com>
2025-12-10 05:01:40 +0000

    Add files via upload

---
+++ Themes/ThemesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows;
+
+namespace Spark
+{
+	public static class ThemesController
+	{
+		public enum ThemeTypes
+		{
+			YellowDark,
+			OrangeDark,
+			RedDark
+		}
+
+		public static ThemeTypes CurrentTheme { get; set; }
+
+		private static ResourceDictionary ThemeDictionary
+		{
+			get { return Application.Current.Resources.MergedDictionaries[0]; }
+			set { Application.Current.Resources.MergedDictionaries[0] = value; }
+		}
+
+		private static void ChangeTheme(Uri uri)
+		{
+			ThemeDictionary = new ResourceDictionary { Source = uri };
+		}
+
+		public static void SetTheme(ThemeTypes theme)
+		{
+			CurrentTheme = theme;
+			string themeName = theme switch
+			{
+				ThemeTypes.YellowDark => "ColourfulDarkTheme_Yellow",
+				ThemeTypes.OrangeDark => "ColourfulDarkTheme_Orange",
+				ThemeTypes.RedDark => "ColourfulDarkTheme_Red",
+				_ => null
+			};
+
+			try
+			{
+				if (!string.IsNullOrEmpty(themeName))
+				{
+					ChangeTheme(new Uri($"Themes/{themeName}.xaml", UriKind.Relative));
+				}
+			}
+			catch (Exception e)
+			{
+				Logger.Error($"Error changing the theme.\n{e}");
+				// ignored
+			}
+		}
+	}
+}
